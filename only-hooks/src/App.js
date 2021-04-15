d2632b1f93f0d21ecadecee5f7bc1d62ef9926d1
Lamanris <sprite_kg@mail.ru>
2021-04-15 20:26:01 +0400

    only-hooks done

--- only-hooks/src/App.js
+++ only-hooks/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Header from "./components/Header";
 import ContactsList from "./components/ContactsList";
@@ -8,11 +10,28 @@ import AddContact from "./components/AddContact";
 const App = () => {
     const [search, setSearch] = useState('')
     const [contacts, setContacts] = useState([])
+    const [isFetched, setIsFetched] = useState(false)
     const [showAddContact, setShowAddContact] = useState(false)
+
+    const notify = (status) => {
+        toast.success(`🦄 Successfully ${status ? 'added' : 'deleted'}!`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        })
+    }
+    const toastContainer =  <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover/>
 
     useEffect(() => {
         axios('https://6076ba351ed0ae0017d69a16.mockapi.io/contacts')
-            .then(({data}) => setContacts(data))
+            .then(({data}) => {
+                setContacts(data)
+                setIsFetched(true)
+            })
     },[])
 
     function deleteContact (id) {
@@ -20,6 +39,7 @@ const App = () => {
             .then(({data}) => {
                 setContacts(contacts.filter(el => el.id !== data.id))
                 console.log('Deleted contact: ', data)
+                notify(false)
             })
     }
 
@@ -28,6 +48,7 @@ const App = () => {
             .then(({data}) => {
                 setContacts([...contacts, data])
                 console.log('New contact: ', data)
+                notify(true)
             })
     }
 
@@ -35,7 +56,8 @@ const App = () => {
         <div className="w-1/3 mx-auto my-4">
             <Header setSearch={setSearch} setShowAddContact={setShowAddContact} showAddContact={showAddContact}/>
             {showAddContact && <AddContact addNewContact={addNewContact} setShowAddContact={setShowAddContact}/>}
-            <ContactsList contacts={contacts} search={search} deleteContact={deleteContact}/>
+            <ContactsList contacts={contacts} search={search} deleteContact={deleteContact} isFetched={isFetched}/>
+            {toastContainer}
         </div>
     );
 };
--- only-hooks/src/components/ContactsList.js
+++ only-hooks/src/components/ContactsList.js
@@ -3,20 +3,20 @@ import HashLoader from "react-spinners/HashLoader";
 
 import ContactsListItem from "./ContactsListItem";
 
-const ContactsList = ({contacts, search, deleteContact}) => {
+const ContactsList = ({contacts, search, deleteContact, isFetched}) => {
     const filteredList = contacts.filter(el => el.name.toLowerCase().includes(search.toLowerCase()))
     return (
         <div className="shadow-md overflow-y-scroll" style={{height: '500px'}}>
             { !filteredList.length && contacts.length ? <h2 className="p-4 text-xl">Looks like there is no such contact...</h2> : null }
+            {isFetched && !contacts.length && <h2 className="p-4 text-xl">No contacts here... Please add some.</h2>}
             {
-                contacts.length > 0 ? (
-                    filteredList.map(el => <ContactsListItem user={el} key={el.id} deleteContact={deleteContact}/>)
-                ) : (
+                !contacts.length && !isFetched ? (
                     <div className="flex justify-center items-center h-full">
                         <HashLoader size={150} color={"#60a5fa"}/>
                     </div>
+                ) : (
+                    filteredList.map(el => <ContactsListItem user={el} key={el.id} deleteContact={deleteContact}/>)
                 )
-
             }
         </div>
     );
